724a2ea766f67835f1a52b6431ba3283d839c853
BeConOr <selig2014@yandex.ru>
2022-10-09 23:13:20 +0700

    Add client starting.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "Server/src/ClientPart.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int dataSocket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (dataSocket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server::ClientPart client(dataSocket);
+    client.connect();
+    client.start();
     return 0;
 }
